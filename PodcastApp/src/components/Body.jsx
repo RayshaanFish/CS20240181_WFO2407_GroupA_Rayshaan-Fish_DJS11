f221d1a00dc5698a062141db1dd4515b8950bd36
Rayshaan Fish <Rayshaan94@gmail.com>
2024-11-22 14:16:31 +0200

    added Body & Footer components

---
+++ PodcastApp/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../components/Body.css";
+
+function Body() {
+  return (
+    <body className="body">
+      <p>This is the body</p>
+    </body>
+  );
+}
+
+export default Body;
---
+++ PodcastApp/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../components/Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Podcast App. All right reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
