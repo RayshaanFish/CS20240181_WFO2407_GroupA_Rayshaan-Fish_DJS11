e9f05bb58e8e5d7e6cb3c8db4a9913faf1efcdd1
Rayshaan Fish <Rayshaan94@gmail.com>
2024-11-22 14:53:04 +0200

    added routes, Header, About, Search; rename routes as needed

--- PodcastApp/src/App.jsx
+++ PodcastApp/src/App.jsx
@@ -1,8 +1,42 @@
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+
 import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <div className="app">
+          <Header />
+          <body className="body">
+            <Routes>
+              {/* Routes for different pages */}
+              <Route path="/" element={<Body />} />
+              <Route
+                path="/about"
+                element={<p>Still need to add this Page [ABOUT]</p>}
+              />
+              <Route
+                path="/search"
+                element={<p>Still need to add this Page [SEARCH]</p>}
+              />
+            </Routes>
+          </body>
+          <Footer />
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- PodcastApp/src/components/Header.jsx
+++ PodcastApp/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import "../components/Header.css";
 
 function Header() {
   return (
     <header className="header">
-      <h1>Header</h1>
+      <h1>THIS IS A HEADER</h1>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/search">Search</Link>
+      </nav>
     </header>
   );
 }
